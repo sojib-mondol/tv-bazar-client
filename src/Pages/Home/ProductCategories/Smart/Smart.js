927f6ec8c8f5dcb3d409bceefc020eedb0ae157c
sojib-sudo <sojibmandol@gmail.com>
2022-11-25 21:56:18 +0600

    smart tv data loaded from server

--- src/Pages/Home/ProductCategories/Smart/Smart.js
+++ src/Pages/Home/ProductCategories/Smart/Smart.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import TvCard from '../Crt/TvCard';
 
 const Smart = () => {
+    const [tvs, settvs] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/smartTvCollection')
+        .then(res => res.json())
+        .then(data => settvs(data))
+    }, [])
     return (
         <div>
-            <h2>This is smart</h2>
+            <div className='text-center mb-4'>
+                <p className="text-2xl font-bold text-orange-600">SMART TV</p>
+                <h2 className="text-5xl font-semibold">Our SMART TV Collection</h2>
+                <p>We sell & buy good qualitiful SMART TV with a good deal.</p>
+            </div>
+            <div>
+                {/* <h2>services: {services.length}</h2> */}
+                <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                    {
+                        tvs.map(tv => <TvCard
+                            key={tv._id}
+                            tv={tv}
+                        ></TvCard>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
